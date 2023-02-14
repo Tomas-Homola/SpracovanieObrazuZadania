7cbb7dcc2acf39ddb59f756e43f1d9ee2c5b5f82
Tomas-Homola <homola.tom6@gmail.com>
2023-02-14 15:52:52 +0100

    Vytvorenie repozitara

---
+++ src/IPmodul.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// asi trieda "IPmodul", ktora bude mat funkcie na Image Processing
+// na vstup do konstruktora dostane data, s ktorymi sa bude pracovat
+// double* m_localData
+// funkcia na zrkadlenie a odzrkadlenie -> mirror(int N), rozsiri obrazok o N pixelov kazdym smerom tak, ze prekopiruje hodnoty pixelov pri hranici obrazka
